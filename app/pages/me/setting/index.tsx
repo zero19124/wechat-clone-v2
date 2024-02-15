57f79465b4e6c69937790480fc81be061ad14c62
Evan <Zhuanz@EvanMBP.lan>
2024-02-16 01:46:20 +0800

    add theme demo

--- app/pages/me/setting/index.tsx
+++ app/pages/me/setting/index.tsx
@@ -1,28 +1,49 @@
 import { Button, Text, View } from "react-native";
 import { useTranslation } from "react-i18next";
-import { useLayoutEffect } from "react";
+import { useEffect, useLayoutEffect } from "react";
 import { useNavigation } from "expo-router";
+import * as light from "@/theme/light";
+import { useTheme } from "@/theme/useTheme";
+import { NativeStackNavigationOptions } from "@react-navigation/native-stack";
 
 const Setting = () => {
   const navigator = useNavigation();
   const { t } = useTranslation();
   const { i18n } = useTranslation();
+  const { toggleTheme, themeName, themeColor } = useTheme();
+
   const handleChangeLanguage = (language: string) => {
     i18n.changeLanguage(language);
   };
 
   useLayoutEffect(() => {
     navigator.setOptions({
-      title: t("setting"),
-    });
+      headerTitle: () => (
+        <Text style={{ color: themeColor.text5, fontSize: 20 }}>
+          {t("setting")}
+        </Text>
+      ),
+      headerTintColor: themeColor.text5,
+      headerShadowVisible: false,
+      headerStyle: { backgroundColor: themeColor.white },
+    } as NativeStackNavigationOptions);
+  });
+  useEffect(() => {
+    console.log(222, themeColor.white);
   });
   return (
-    <View>
+    <View style={{ backgroundColor: themeColor.white, flex: 1 }}>
       <Button
+        color={themeColor.text5}
         onPress={() => {
           handleChangeLanguage(i18n.language === "cn" ? "en" : "cn");
         }}
         title={t("change language to chinese")}
+      />
+      <Button
+        color={themeColor.text5}
+        onPress={() => toggleTheme(themeName === "light" ? "dark" : "light")}
+        title={t("change the theme")}
       />
     </View>
   );
