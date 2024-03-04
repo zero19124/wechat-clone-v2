d82b903b39664a33874dea8c2f52509d77754b8e
Evan <Zhuanz@EvanMBP.local>
2024-03-04 09:08:17 +0800

    implementing map

--- app/pages/me/index.tsx
+++ app/pages/me/index.tsx
@@ -236,4 +236,27 @@ const Me = () => {
     </SafeAreaView>
   );
 };
-export default Me;
+
+import React from "react";
+import MapView from "react-native-maps";
+import { StyleSheet } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <MapView style={styles.map} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
+
+// export default Me;
